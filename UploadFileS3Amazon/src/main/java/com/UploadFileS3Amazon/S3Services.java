893ce73b68883d60b06e0cda2e855c637df2369c
Snita1 <shakyasnita19@gmail.com>
2019-08-08 18:18:54 -0400

    Spring boot App

---
+++ UploadFileS3Amazon/src/main/java/com/UploadFileS3Amazon/S3Services.java
@@ -0,0 +1,12 @@
+package com.UploadFileS3Amazon;
+
+import java.io.ByteArrayOutputStream;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface S3Services {
+	
+	public ByteArrayOutputStream downloadFile(String keyName);
+	  public void uploadFile(String keyName, MultipartFile file);
+
+}
---
+++ UploadFileS3Amazon/src/main/java/com/UploadFileS3Amazon/UploadFileS3AmazonApplication.java
@@ -0,0 +1,13 @@
+package com.UploadFileS3Amazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadFileS3AmazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadFileS3AmazonApplication.class, args);
+	}
+
+}
